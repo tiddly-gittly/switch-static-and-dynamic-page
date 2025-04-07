e3a0ed6d3c6881e9660dc506dece384fa773476d
linonetwo <linonetwo012@gmail.com>
2025-04-07 20:15:22 +0800

    chore: update eslint version

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import tidgiConfig from 'eslint-config-tidgi';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default [
+  ...tidgiConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['./*.js', './*.mjs'],
+        },
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      'no-var': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+      '@typescript-eslint/restrict-template-expressions': ['warn', { allowNumber: true }],
+    },
+  },
+];
